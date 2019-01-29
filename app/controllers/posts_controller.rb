67696767a292eee61b2b86895d4a8734517b8d2f
Zamiz <zamiztobar@gmail.com>
2019-01-29 16:18:41 -0300

    model: add read permission to ability add load_and_authorize_resource to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  #before_action :set_post, only: [:show, :edit, :update, :destroy]
+  #authorize_resource
+  load_and_authorize_resource
   # GET /posts
   # GET /posts.json
   def index
@@ -56,7 +57,6 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    authorize! :destroy, @post
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
@@ -66,9 +66,9 @@ class PostsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+    # def set_post
+    #   @post = Post.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
